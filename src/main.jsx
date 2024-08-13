2d89c948f34243747227ceea9f896f678cef1520
HarshitX1002 <singhharshit552@gmail.com>
2024-08-13 19:48:16 +0530

    browser commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from "react"
+import { BrowserRouter, Routes, Route } from "react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter
       basename="/iGraft-landingpage/"></BrowserRouter>
-
-    <App />
+    <Routes>
+      <Route path="*" element={<App />} />
+    </Routes>
   </React.StrictMode>,
 )
